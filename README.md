ef5fb96b529fd936d0bd2adc3a1c39f47748016a
yaikohi <erik.beem@hotmail.com>
2022-07-09 02:20:19 +0200

    Setup astro

--- README.md
+++ README.md
@@ -21,4 +21,3 @@ bun dev
 Open http://localhost:3000 with your browser to see the result.
 
 You can start editing the page by modifying src/App.jsx. The page auto-updates as you edit the file.
-
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./public/**/*.html",
+    "./src/**/*.{astro,md,js,jsx,ts,tsx,svelte,vue}",
+    "../../libs/**/*.{astro,md,js,jsx,ts,tsx,svelte,vue}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
