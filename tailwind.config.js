47aaf587a01b42ff9df3e955ce272536100c172e
yaikohi <erik.beem@hotmail.com>
2022-07-12 17:22:41 +0200

    Update tailwindconfig & global css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@ module.exports = {
   content: [
     "./public/**/*.html",
     "./src/**/*.{astro,md,js,jsx,ts,tsx,svelte,vue}",
-    "../../libs/**/*.{astro,md,js,jsx,ts,tsx,svelte,vue}"
+    "./src/**/**/*.{astro,md,js,jsx,ts,tsx,svelte,vue}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
